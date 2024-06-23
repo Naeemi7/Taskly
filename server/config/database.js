6535ad0705a63a82d66aca5ff040d5e8237202b2
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-06-24 00:46:56 +0200

    the database connection has been made

---
+++ server/config/database.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectToMongoDB = async () => {
+  const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+
+  if (!DB_USER || !DB_PASS || !DB_HOST || !DB_NAME) {
+    console.error(
+      "Missing necessary environment variables for MongoDB connection"
+    );
+    return;
+  }
+
+  const mongoURI = `mongodb+srv://${DB_USER}:${encodeURIComponent(
+    DB_PASS
+  )}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected! 👍");
+  } catch (error) {
+    console.log("Database connection error:", error.message);
+  }
+};
+
+export default connectToMongoDB;
--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 
 // Internal imports
 import app from "./app.js";
+import connectToMongoDB from "./config/database.js";
 
 // Define the PORT variable
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log("Server is listening on port ", PORT);
-});
+connectToMongoDB().then(() =>
+  app.listen(PORT, () => {
+    console.log("Server is listening on port ", PORT);
+  })
+);
