4a4846740767c2bb6b45e4c19ee65b743c55aaa9
Abdulwase Naeemi <abdulwasenaeemi7@gmail.com>
2024-06-27 19:07:09 +0200

    the problem with handling both toast and alertbox has been fixed

--- client/src/utils/errorUtils.js
+++ client/src/utils/errorUtils.js
@@ -12,6 +12,9 @@ export const logError = (message = "Error", error) => {
   }
 };
 
+// Set to track shown error message
+const shownErrors = new Set();
+
 export const handleError = (error, setError) => {
   let errorMessage = "";
 
@@ -37,7 +40,16 @@ export const handleError = (error, setError) => {
         errorMessage = data.error || "An error occurred";
     }
   }
-
   setError(errorMessage); // Set error message to state
-  ShowToast(errorMessage, "error"); // Show toast with the error message
+
+  // Check if the error message has been shown already
+  if (!shownErrors.has(errorMessage)) {
+    ShowToast(errorMessage, "error"); // Show toast with the error message
+    shownErrors.add(errorMessage); // Add the error message to the set
+  } else {
+    // Clear shownErrors after 5 seconds (adjust as needed)
+    setTimeout(() => {
+      shownErrors.delete(errorMessage);
+    }, 5000);
+  }
 };
